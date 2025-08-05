30f89b9921134428e0bb0bce28fa4ab2dcae66da
Fernando IL <luffynando@gmail.com>
2025-08-05 08:59:37 -0600

    chore: :recycle: update file contstants

--- src/cli/constants_generated.ts
+++ src/cli/constants_generated.ts
@@ -1,5 +1,5 @@
 export const versionsMap = {
-  '@adonisjs/eslint-plugin': '^2.0.0',
+  '@adonisjs/eslint-plugin': '^2.0.1',
   '@nuxt/eslint-plugin': '^1.7.1',
   'astro-eslint-parser': '^1.2.2',
   'eslint': '^9.32.0',
