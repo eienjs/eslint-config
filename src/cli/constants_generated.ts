d4bba3c6d3cafece084769ed7790926527d4faab
Fernando IL <luffynando@gmail.com>
2025-09-22 16:25:36 -0600

    chore: :arrow_up: update mappep generated contants versions

--- src/cli/constants_generated.ts
+++ src/cli/constants_generated.ts
@@ -1,9 +1,9 @@
 export const versionsMap = {
   '@adonisjs/eslint-plugin': '^2.0.1',
-  '@nuxt/eslint-plugin': '^1.8.0',
+  '@nuxt/eslint-plugin': '^1.9.0',
   'astro-eslint-parser': '^1.2.2',
-  'eslint': '^9.32.0',
+  'eslint': '^9.36.0',
   'eslint-plugin-astro': '^1.3.1',
-  'eslint-plugin-format': '^1.0.1',
+  'eslint-plugin-format': '^1.0.2',
   'prettier-plugin-astro': '^0.14.1',
 };
