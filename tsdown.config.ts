906619fb6b6f271329b1bc65f4c1ca169b7b2038
Fernando IL <luffynando@gmail.com>
2025-09-22 16:39:45 -0600

    chore: :bug: resolve correct generation of build and changelog

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -5,5 +5,8 @@ export default defineConfig([
     entry: ['src/index.ts', 'src/cli/index.ts', 'src/configs/index.ts'],
     shims: true,
     format: ['esm'],
+    platform: 'node',
+    exports: true,
+    unbundle: true,
   },
 ]);
