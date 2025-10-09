662307d7e2bcaa6cf01b848b0a4b80162bb16999
Fernando Isidro L <luffynando@gmail.com>
2025-10-09 16:52:31 -0600

    chore: :construction: add target on config tsdown

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -5,6 +5,7 @@ export default defineConfig([
     entry: ['src/index.ts', 'src/cli/index.ts', 'src/configs/index.ts'],
     shims: true,
     format: ['esm'],
+    target: 'esnext',
     platform: 'node',
     exports: true,
     unbundle: true,
