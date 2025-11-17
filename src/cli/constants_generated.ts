22d049d492ca0bfd7f8c62a21756cc3e7b62cb3c
Fernando Isidro L <luffynando@gmail.com>
2025-11-16 20:13:07 -0600

    chore: :arrow_up: update dependencies

--- src/cli/constants_generated.ts
+++ src/cli/constants_generated.ts
@@ -3,7 +3,7 @@ export const versionsMap = {
   '@nuxt/eslint-plugin': '^1.10.0',
   'astro-eslint-parser': '^1.2.2',
   'eslint': '^9.39.1',
-  'eslint-plugin-astro': '^1.4.0',
+  'eslint-plugin-astro': '^1.5.0',
   'eslint-plugin-format': '^1.0.2',
   'prettier-plugin-astro': '^0.14.1',
 };
