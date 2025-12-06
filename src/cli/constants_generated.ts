a05f187d1172cf7d050c9b0dbbc7f6b15289899b
Fernando Isidro L <luffynando@gmail.com>
2025-12-06 17:43:11 -0600

    chore: :arrow_up: update eslint-plugin-format to version 1.1.0

--- src/cli/constants_generated.ts
+++ src/cli/constants_generated.ts
@@ -4,6 +4,6 @@ export const versionsMap = {
   'astro-eslint-parser': '^1.2.2',
   'eslint': '^9.39.1',
   'eslint-plugin-astro': '^1.5.0',
-  'eslint-plugin-format': '^1.0.2',
+  'eslint-plugin-format': '^1.1.0',
   'prettier-plugin-astro': '^0.14.1',
 };
