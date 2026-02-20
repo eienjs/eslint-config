d28dbd9b70d914862f7991c9978c2bf3503ad712
Fernando IL <luffynando@gmail.com>
2026-02-20 14:16:58 -0600

    chore: :arrow_up: update constants for latest libs versions

--- src/cli/constants_generated.ts
+++ src/cli/constants_generated.ts
@@ -1,9 +1,9 @@
 export const versionsMap = {
-  '@adonisjs/eslint-plugin': '^2.2.1',
-  '@nuxt/eslint-plugin': '^1.13.0',
-  'astro-eslint-parser': '^1.2.2',
-  'eslint': '^9.39.2',
-  'eslint-plugin-astro': '^1.5.0',
-  'eslint-plugin-format': '^1.3.1',
+  '@adonisjs/eslint-plugin': '^2.2.2',
+  '@nuxt/eslint-plugin': '^1.15.1',
+  'astro-eslint-parser': '^1.3.0',
+  'eslint': '^9.39.3',
+  'eslint-plugin-astro': '^1.6.0',
+  'eslint-plugin-format': '^1.4.0',
   'prettier-plugin-astro': '^0.14.1',
 };
