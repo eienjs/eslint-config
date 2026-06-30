2d06bed11989cd2527898e7161b15c62065f75b3
Fernando Isidro L <luffynando@gmail.com>
2026-06-29 21:13:53 -0600

    build: :white_check_mark: update generate files for versions map

--- src/cli/constants_generated.ts
+++ src/cli/constants_generated.ts
@@ -2,7 +2,7 @@ export const versionsMap = {
   '@adonisjs/eslint-plugin': '^2.2.2',
   '@nuxt/eslint-plugin': '^1.16.0',
   'astro-eslint-parser': '^2.1.0',
-  'eslint': '^10.5.0',
+  'eslint': '^10.6.0',
   'eslint-plugin-astro': '^2.1.1',
   'eslint-plugin-format': '^2.0.1',
   'prettier-plugin-astro': '^0.14.1',
