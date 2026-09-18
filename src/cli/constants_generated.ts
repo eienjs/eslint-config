c1cf96e95a0721753207dd1798fbba6d2d0ea89f
Fernando IL <luffynando@gmail.com>
2026-09-18 09:15:13 -0600

    chore: :arrow_up: bump dependency versions in versionsMap

--- src/cli/constants_generated.ts
+++ src/cli/constants_generated.ts
@@ -1,9 +1,9 @@
 export const versionsMap = {
   '@adonisjs/eslint-plugin': '^2.2.2',
-  '@nuxt/eslint-plugin': '^1.16.0',
-  'astro-eslint-parser': '^2.1.0',
-  'eslint': '^10.6.0',
-  'eslint-plugin-astro': '^2.1.1',
+  '@nuxt/eslint-plugin': '^1.17.0',
+  'astro-eslint-parser': '^3.1.0',
+  'eslint': '^10.10.0',
+  'eslint-plugin-astro': '^3.2.1',
   'eslint-plugin-format': '^2.0.1',
-  'prettier-plugin-astro': '^0.14.1',
+  'prettier-plugin-astro': '^1.0.1',
 };
